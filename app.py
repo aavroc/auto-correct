28e3e012b3d318323686356820a90380beefe23d
Max Bisschop <max@ffkijken.com>
2023-06-13 17:33:35 +0200

    init2

--- app.py
+++ app.py
@@ -1,33 +1,39 @@
 # app.py
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from canvasapi import Canvas
 import requests
 import sys
 from pathlib import Path
+import os
+import json
+from datetime import datetime
+import glob
+
+from myflaskapp.mydata import data
+from myflaskapp.config import config
+
+from controls.section import section
 
 app = Flask(__name__)
 
 # Canvas API URL
-API_URL = "https://talnet.instructure.com"
+API_URL = config['API_URL']
 # Canvas API key
-API_KEY = "17601~V6FUc7Xvc07XkCMxJtDVJlpN7RiugCbaodIJ6pUzfnxTZ7S44eRs7yGW7jKc0hOO"
+API_KEY = config['API_KEY']
+default_feedback = config['default_feedback']
 
-feedback = 'Top gedaan!'
-
-data = [
-    {"course_id": 6585, "assignment_id": 117166, "rating":10, 'file_type':'php', "words_in_order": ["localhost", "games", "root","localhost", "artiesten", "roc-student", "welkom123", "$conn2" ], 'feedback':feedback},
-    {"course_id": 6585, "assignment_id": 117167, "rating":10, 'file_type':'php', "words_in_order": ["php", "localhost", "insert", "into", "producten", "values", "Playstation "], 'feedback':feedback},
-    {"course_id": 10780, "assignment_id": 136418, "rating":10, 'file_type':'js', "words_in_order": ["changeColor()","red", "getNumbers()", "this.state","addStar()", "render()", "onClick"], 'feedback':feedback},
-    {"course_id": 7760, "assignment_id": 131584, "rating":10, 'file_type':'php', "words_in_order": ["php", "countries", "country","number_format", "country->SurfaceArea", "2"], 'feedback':feedback},
-    {"course_id": 6585, "assignment_id": 117603, "rating":10, 'file_name':'verwijder' ,'file_type':'php', "words_in_order": ["php", "database", "delete","from", "klanten", "where","query"], 'feedback':feedback},
-]
-
-
+app.register_blueprint(section, url_prefix="")
 
 # Initialize a new Canvas object
 canvas = Canvas(API_URL, API_KEY)
 # Initialize a new Canvas object
 canvas = Canvas(API_URL, API_KEY)
+
+def removeTempFiles():
+    path='static/temp/*'
+    files = glob.glob(path)
+    for f in files:
+        os.remove(f)
 
 def rateSubmission(submission, rating, feedback, test=1):
     if (test):
@@ -37,7 +43,7 @@ def rateSubmission(submission, rating, feedback, test=1):
         #submission.edit(submission={'posted_grade': rating})
         #submission.edit(comment= {'text_comment': feedback}))
 
-def listAssignments():
+def moved_listAssignments():
     # Create a list of available assignments that can be auto graded
     list_of_dicts=[]
     for item in data:
@@ -51,16 +57,16 @@ def listAssignments():
             print(f"No access to course {course_id} and/or assignment {assignment_id}")
 
         # Get all submissions
-        submissions = assignment.get_submissions()
+        # submissions = assignment.get_submissions()
 
-        list_of_dicts.append({'course_id':course_id, 'course_name':course.name,'assignment_id':assignment_id,'assignment_name':assignment.name})
+        list_of_dicts.append({'course_id':course_id, 'course_name':course.name,'assignment_id':assignment_id,'assignment_name':assignment.name, 'points_possible':assignment.points_possible,})
 
     return(list_of_dicts)
 
 def listUnratedAssignments(item):
     course_id=item['course_id']
     assignment_id=item['assignment_id']
-    rating=item['rating']
+    rating=item.get('rating','0') # changed, getting from assignemtn (points_possible)
     words_in_order=item['words_in_order']
     file_type=item['file_type']
     file_name=item.get('file_name', "")
@@ -76,8 +82,10 @@ def listUnratedAssignments(item):
     # Get all submissions
     submissions = assignment.get_submissions(include=['user'])
 
+    removeTempFiles()
+
     # Process each submission
-    for submission in submissions:
+    for i, submission in enumerate(submissions):
         # Check if the submission is already graded
         if submission.submitted_at is None or submission.workflow_state == 'graded':
             continue
@@ -85,37 +93,53 @@ def listUnratedAssignments(item):
         for attachment in submission.attachments:
             if Path(attachment.filename).suffix != '.'+file_type: # does the filename extentsion mach the required one?
                 continue
-            print(f"FN: {file_name} == {attachment.filename}\n")
+
             if ( file_name != '' and file_name not in attachment.filename ): # when defined, check if the filename is correct.
                 continue
             response = requests.get(attachment.url, allow_redirects=True)
             if response.status_code != 200:
                 print(f"Failed to download file: {attachment['url']}")
                 continue
-
-            file_content = response.content.decode()
-
-            pos = 0
-            words_correct = 0
-            for word in words_in_order:
-                pos = file_content.lower().find(word.lower(), pos)
-                if pos == -1:
-                    # Word not found, stop searching
-                    break
-                else:
-                    words_correct+=1
-
-            if (pos > 1):
-                rating=item['rating']
-                feedback=item['feedback']
+            
+            if ( file_type == 'png' ):
+                #save file
+                path='static/temp'
+                page = requests.get(attachment.url)
+                if not os.path.exists(path):
+                    os.makedirs(path)
+                # png_filename = attachment.filename
+                png_filename = datetime.now().strftime('%H%M%S%f')+'-'+str(i)+'.png'
+                fn = os.path.join(path,png_filename)
+                with open(fn, 'wb') as f:
+                    f.write(page.content)
+                file_content = 'fn:'+fn
+                rating=item.get('rating','')
+                feedback=item.get('feedback',default_feedback)
+                words_correct=-99
             else:
-                rating=0
-                feedback='Niet helemaal goed'
+                file_content = response.content.decode()
+
+                pos = 0
+                words_correct = 0
+                for word in words_in_order:
+                    pos = file_content.lower().find(word.lower(), pos)
+                    if pos == -1:
+                        # Word not found, stop searching
+                        break
+                    else:
+                        words_correct+=1
+
+                if (pos > 1):
+                    rating=item.get('rating','0')
+                    feedback=item.get('feedback',default_feedback)
+                else:
+                    rating=0
+                    feedback='Niet helemaal goed'
 
             list_of_dicts.append({'assignment_id':assignment_id,'assignment_name':assignment.name,
             'course_id':course.id,'course_name':course.name,'submission_id':submission.id,
-            'rating':rating, 'feedback':feedback,'user':submission.user['name'], 'file_content':file_content,
-            'words_in_order':words_in_order,'words_correct':words_correct})
+            'rating':assignment.points_possible, 'feedback':feedback,'user':submission.user['name'], 'file_content':file_content,
+            'words_in_order':words_in_order,'words_correct':words_correct,'hint':item.get('hint','')})
 
     return(list_of_dicts)
 
@@ -165,9 +189,8 @@ def autoGrade(course_id, assignment_id, rating, search_words):
                 print(f"Not OK {pos}")
                 print(f"Submission incorrect, not grading student: {submission.user['name']} (ID: {submission.user_id})")
 
-
 def update_grade_and_feedback(posted_variables):
-    output='<pre>'
+    output=''
     rating_values = request.form.getlist('rating[]')
     feedback_values = request.form.getlist('feedback[]')
     checked_values = request.form.getlist('checked[]')
@@ -175,8 +198,6 @@ def update_grade_and_feedback(posted_variables):
     assignment_id_values = request.form.getlist('assignment_id[]')
     course_id_values = request.form.getlist('course_id[]')
     check_values = request.form.getlist('checked[]')
-
-    print(f"checked_values")
 
     # We have 6 lists with all the saem length. All lists will have the same course_id and assignment_id
 
@@ -196,20 +217,32 @@ def update_grade_and_feedback(posted_variables):
             # output+=(f"Evaluating {submission.id} and {submission_id}\n")
             if ( int(submission.id) == int(submission_id) and int(check_values[i])==1 ):
                     count+=1
-                    output+=(f"\nRating {submission_id} for {submission.user['name']} with rate {rating_values[i]} and feedback {feedback_values[i]}")
+                    output+=(f"\nRating {submission_id} for {submission.user['name']} with rate {rating_values[i]} and feedback {feedback_values[i]}\n")
                     # next two lines do the actual rating and feedback submision
                     submission.edit(submission={'posted_grade': str(rating_values[i])})
                     submission.edit(comment={'text_comment': feedback_values[i]})
 
     output+=(f"\n\nRated {count} assignments succesfully.")
-    output+="</pre>"
     return(output)
 
 
-@app.route('/')
-def index():
-    results=listAssignments()
-    return render_template('index.html', data=results)
+@app.route('/moved-refresh-list')
+def moved_refresh_list():
+    
+    data = data = listAssignments()
+
+    filename = 'data.json'
+    if os.path.isfile(filename): os.remove(filename)
+
+    with open('filename', 'w') as f:  # open file in write mode
+        json.dump(data, f)  # write data to file in JSON format
+
+    return redirect(url_for('index'))
+
+
+@app.route('/moved')
+def moved_index():
+    section()
 
 @app.route('/correct/<assignment_id>')
 def correct(assignment_id):
@@ -221,12 +254,12 @@ def correct(assignment_id):
 
 @app.route('/submit-ratings', methods=['POST'])
 def submit_ratings():
-    result=update_grade_and_feedback(request.form)
-    return(result)
+    results=update_grade_and_feedback(request.form)
+    return render_template('results.html', data=results)
 
 @app.route('/test')
 def test():
-    return render_template('test.html')
+    return render_template('results.html', data='Empty data set')
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ myflaskapp/mydata.py
@@ -0,0 +1,36 @@
+feedback='Top job!'
+
+data = [
+    {"course_id": 6585, "assignment_id": 117166, "rating":10,  'file_type':'php', "words_in_order": ["localhost", "games", "root","localhost", "artiesten", "roc-student", "welkom123", "$conn2" ], 'feedback':feedback},
+    {"course_id": 6585, "assignment_id": 117167, "rating":10,  'file_type':'php', "words_in_order": ["php", "localhost", "insert", "into", "producten", "values", "Playstation "], 'feedback':feedback},
+    
+    {"course_id": 6585, "assignment_id": 117603, "rating":10,  'file_name':'verwijder' ,'file_type':'php', "words_in_order": ["php", "database", "delete","from", "klanten", "where","query"], 'feedback':feedback},
+    
+    {"course_id": 7760, "assignment_id": 131583, "rating":5,  'file_type':'php', "words_in_order": ["php", "function", "dd","function", "die"], 'feedback':feedback},
+    {"course_id": 7760, "assignment_id": 131584, "rating":10, 'file_type':'php', "words_in_order": ["php", "countries", "country","number_format", "country->SurfaceArea", "2"], 'feedback':feedback},
+    {"course_id": 7760, "assignment_id": 131586, "rating":5,  'file_type':'png', "words_in_order": [], 'feedback':feedback},
+    {"course_id": 7760, "assignment_id": 131581, "rating":10, 'file_type':'php', "words_in_order": ['php', 'countries', 'country', 'hoofdstad'], 'feedback':feedback},
+    {"course_id": 7760, "assignment_id": 131585, "rating":6,  'file_type':'png', "words_in_order": [], 'feedback':feedback},
+    {"course_id": 7760, "assignment_id": 131587, "rating":6,  'file_type':'png',  "words_in_order": [], 'feedback':feedback},
+
+    {"course_id": 10780, "assignment_id": 136418, "rating":10, 'file_type':'js',  "words_in_order": ["changeColor()","red", "getNumbers()", "this.state","addStar()", "render()", "onClick"], 'feedback':feedback},
+]
+
+#blok7
+data =[
+    {"assignment_id":"131583","mpos":"75","apos":"1",'file_type':'php', "words_in_order": ["php", "function", "dd","function", "die"],"assignment_naam":"Debug ready","module_id":"29348","module_naam":"Yii - Level 2","course_id":"7760","korte_naam":"B7","course_naam":"Blok 7"},
+    {"assignment_id":"131580","mpos":"75","apos":"3",'file_type':'png', "words_in_order": [],"assignment_naam":"Bevolingsdichtheid","module_id":"29348","module_naam":"Yii - Level 2","course_id":"7760","korte_naam":"B7","course_naam":"Blok 7"},
+    {"assignment_id":"131588","mpos":"75","apos":"4",'file_type':'png', "words_in_order": [],"assignment_naam":"Menu (oops)","module_id":"29348","module_naam":"Yii - Level 2","course_id":"7760","korte_naam":"B7","course_naam":"Blok 7"},
+    {"assignment_id":"131592","mpos":"75","apos":"6",'file_type':'php', "words_in_order": ['foreach','countries ','Population','td','tr','table'],"assignment_naam":"View","module_id":"29348","module_naam":"Yii - Level 2","course_id":"7760","korte_naam":"B7","course_naam":"Blok 7"},
+    {"assignment_id":"131584","mpos":"75",'file_type':'png', "words_in_order": [],"apos":"7","assignment_naam":"Europa","module_id":"29348","module_naam":"Yii - Level 2","course_id":"7760","korte_naam":"B7","course_naam":"Blok 7"},
+    {"assignment_id":"131590","mpos":"75",'file_type':'png', "words_in_order": [],'hint':'Check op alleen landen in Euopa',"apos":"8","assignment_naam":"Selectie (SurfaceArea < 100000)","module_id":"29348","module_naam":"Yii - Level 2","course_id":"7760","korte_naam":"B7","course_naam":"Blok 7"},
+    {"assignment_id":"131586","mpos":"75",'file_type':'png', "words_in_order": [],"apos":"9","assignment_naam":"Hoofdstad","module_id":"29348","module_naam":"Yii - Level 2","course_id":"7760","korte_naam":"B7","course_naam":"Blok 7"},
+    {"assignment_id":"131581","mpos":"75","apos":"10",'file_type':'php', "words_in_order": ['php', 'countries', 'country', 'hoofdstad'],"assignment_naam":"Clickable hoofdstad","module_id":"29348","module_naam":"Yii - Level 2","course_id":"7760","korte_naam":"B7","course_naam":"Blok 7"},
+    {"assignment_id":"131585","mpos":"75","apos":"11",'file_type':'png', "words_in_order": [],"assignment_naam":"E\u00e9n taal","module_id":"29348","module_naam":"Yii - Level 2","course_id":"7760","korte_naam":"B7","course_naam":"Blok 7"},
+    {"assignment_id":"131587","mpos":"75","apos":"12",'file_type':'png', "words_in_order": [],"assignment_naam":"Meer talen","module_id":"29348","module_naam":"Yii - Level 2","course_id":"7760","korte_naam":"B7","course_naam":"Blok 7"},
+    {"assignment_id":"131589","mpos":"75","apos":"13",'file_type':'png', "words_in_order": [],"assignment_naam":"Percentage","module_id":"29348","module_naam":"Yii - Level 2","course_id":"7760","korte_naam":"B7","course_naam":"Blok 7"},
+    {"assignment_id":"131591","mpos":"75","apos":"14",'file_type':'png', "words_in_order": [],"assignment_naam":"Sorteren","module_id":"29348","module_naam":"Yii - Level 2","course_id":"7760","korte_naam":"B7","course_naam":"Blok 7"},
+]
+
+def testdata():
+    print("OK")
