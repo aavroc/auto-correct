caf8c9849118059a90ef56e6d54cab93e10f0cca
Max Bisschop <max@ffkijken.com>
2023-06-21 15:38:30 +0200

    v 0.6 (Cleaned)

--- app.py
+++ app.py
@@ -7,12 +7,9 @@ from pathlib import Path
 import os
 import json
 import glob
+import random
 
 from myflaskapp.config import config
-from myflaskapp.config import data_cache
-
-from controls.section import section
-#from controls.correct import correct
 
 app = Flask(__name__)
 app.jinja_env.add_extension('jinja2.ext.loopcontrols')
@@ -21,36 +18,115 @@ app.jinja_env.add_extension('jinja2.ext.loopcontrols')
 API_URL = config['API_URL']
 # Canvas API key
 API_KEY = config['API_KEY']
-default_feedback = config['default_feedback']
 
-app.register_blueprint(section, url_prefix="")
-#app.register_blueprint(correct, url_prefix="")
+file_path_form_data = 'static/temp/formdata.json'  # Global variable for the cached form data
 
 # Initialize a new Canvas object
 canvas = Canvas(API_URL, API_KEY)
 # Initialize a new Canvas object
 canvas = Canvas(API_URL, API_KEY)
 
-def removeTempFiles():
+def _removeTempFiles():
     path='static/temp/*'
     files = glob.glob(path)
     for f in files:
         os.remove(f)
 
-def rateSubmission(submission, rating, feedback, test=1):
-    if (test):
-        print(f"Testing (no execution), rating {rating} with feedback {feedback}")
+def loadPicture(url, picture_file_name):
+    path='static/temp/' # save file to temp directory, ToDo when and how to clean temp dir?
+    
+    if not os.path.exists(path):
+        os.makedirs(path)
+   
+    file_name = path+picture_file_name
+
+    if not os.path.isfile(file_name):
+        page = requests.get(url)
+        with open(file_name, 'wb') as f:
+            f.write(page.content)
+    
+    return(file_name)
+
+def validateWords(words, text):
+    pos = 0
+    negative_search = -1
+    words_correct = 0
+
+    for word in words:
+        if word and word[0] == '!':
+            negative_search = text.lower().find(word[1:].lower(), pos)
+        else:
+            pos = text.lower().find(word.lower(), pos)
+
+        if pos == -1 or negative_search != -1:
+            # Word not found, stop searching
+            break
+        else:
+            words_correct+=1
+
+    return words_correct, pos
+
+def saveFormData(data):
+    if os.path.isfile(file_path_form_data):
+        # Rename the existing file with a sequence number
+        sequence_number = 1
+        name, extension = os.path.splitext(file_path_form_data)
+        while True:
+            new_file_path = f"{name}-{sequence_number}{extension}"
+            if not os.path.isfile(new_file_path):
+                os.rename(file_path_form_data, new_file_path)
+                break
+            sequence_number += 1
+
+    with open(file_path_form_data, 'w') as file:
+        json.dump(data, file)
+
+def loadFormData(file_name=''):
+    if file_name == '':
+        file_name = file_path_form_data
     else:
-        print("Here we go...")
-        #submission.edit(submission={'posted_grade': rating})
-        #submission.edit(comment= {'text_comment': feedback}))
+        directory = os.path.dirname(file_path_form_data)
+        file_name = os.path.join(directory, file_name)
+
+    if not os.path.isfile(file_name):  # Check if file exists
+        return False
+    with open(file_name, 'r') as file:
+        data = json.load(file)
+    return data
+
+def getFormdataFiles():
+    directory = os.path.dirname(file_path_form_data)  # Replace with the actual directory path
+
+    # Get all files in the directory
+    files = os.listdir(directory)
+
+    # Filter JSON files with names starting with 'formdata'
+    formdata_files = [f for f in files if f.lower().startswith('formdata') and f.lower().endswith('.json')]
+
+    # Get file creation dates
+    file_data = []
+    for file in formdata_files:
+        file_path = os.path.join(directory, file)
+        creation_date = os.path.getctime(file_path)
+        file_data.append({'name': file, 'creation_date': creation_date})
+
+    # Sort files by creation date
+    file_data.sort(key=lambda x: x['creation_date'])
+
+    return file_data
+
+def getFeedback():
+    #ToDo check if config exists and if it is a list.
+    selected_feedback = random.choice(config['default_feedback'])
+
+    return selected_feedback
 
 def listUnratedAssignments(item):
     course_id=item['course_id']
     assignment_id=item['assignment_id']
     words_in_order=item['words_in_order']
     file_type=item['file_type']
-    file_name=item['file_name']
+    file_name_match=item['file_name']
 
     list_of_dicts=[]
 
@@ -79,7 +155,7 @@ def listUnratedAssignments(item):
             if Path(attachment.filename).suffix.lower() != '.'+file_type.lower(): # does the filename extentsion mach the required one?
                 continue
 
-            if ( file_name != None and file_name not in attachment.filename ): # when defined, check if the filename is correct.
+            if ( file_name_match != None and file_name_match not in attachment.filename ): # when defined, check if the filename is correct.
                 continue
 
             response = requests.get(attachment.url, allow_redirects=True)
@@ -89,21 +165,10 @@ def listUnratedAssignments(item):
             
             if ( file_type.lower() == 'png' ): # png, no word matching
                 
-                path='static/temp/' # save file to temp directory, ToDo when and how to clean temp dir?
-                
-                if not os.path.exists(path):
-                    os.makedirs(path)
-                # png_filename = attachment.filename
-                png_filename = str(course_id)+'-'+str(assignment_id)+'-'+str(submission.id)+'-'+str(attachment.id)+'.png'
-                png_filename = str(course_id)+'-'+str(attachment.id)+'.png'
-                fn = path+png_filename
+                file_name = str(course_id)+'-'+str(attachment.id)+'.png'
+                file_name = loadPicture(attachment.url, file_name) # return file name with path
 
-                if not os.path.isfile(fn):
-                    page = requests.get(attachment.url)
-                    with open(fn, 'wb') as f:
-                        f.write(page.content)
-
-                file_content = 'fn:'+fn # file content refers to a filename fn: <filename> which is the file name to the picture downloaded
+                file_content = 'fn:'+file_name # file content refers to a filename fn: <filename> which is the file name to the picture downloaded
                 feedback=item.get('feedback',default_feedback)
                 words_correct=-99
                 rating = assignment.points_possible # png is not rated automattically, propose higest score.
@@ -112,28 +177,11 @@ def listUnratedAssignments(item):
             else: # anything but a png file, do the word matching
 
                 file_content = response.content.decode()
+                words_correct, position = validateWords(words_in_order, file_content) #validate text(file_conten) with words (words must appear in text in order and !words may not exists in text)
 
-                pos = 0
-                negative_search = -1
-                words_correct = 0
-
-                for word in words_in_order:
-                    if word and word[0] == '!':
-                        print('Neg Search')
-                        negative_search = file_content.lower().find(word[1:].lower(), pos)
-                    else:
-                        print('Pos Search')
-                        pos = file_content.lower().find(word.lower(), pos)
-
-                    if pos == -1 or negative_search != -1:
-                        # Word not found, stop searching
-                        break
-                    else:
-                        words_correct+=1
-
-                if (pos > 1): # pos is position of last found word and will be -1 when a word is not found.
+                if (position > 1): # position of last found word and will be -1 when a word is not found.
                     rating=assignment.points_possible
-                    feedback=item.get('feedback',default_feedback)
+                    feedback=getFeedback()
                 else:
                     rating=0
                     feedback='Niet helemaal goed'
@@ -154,53 +202,9 @@ def listUnratedAssignments(item):
             'hint':item.get('hint',''),
             'png_att_nr':png_att_nr, 'number_of_att':len(submission.attachments)})
 
+    saveFormData(list_of_dicts)
+
     return(list_of_dicts)
-
-def autoGrade(course_id, assignment_id, rating, search_words):
-    try:
-        course = canvas.get_course(course_id)
-        assignment = course.get_assignment(assignment_id)
-    except:
-        print(f"No access to course {course_id} and/or assignment {assignment_id}")
-
-    global feedback
-
-    print("")
-    print(f"Autograde {course.name} {assignment.name}")
-
-    # Get all submissions
-    submissions = assignment.get_submissions(include=['user'])
-
-    # Process each submission
-    for submission in submissions:
-        # Check if the submission is already graded
-        if submission.submitted_at is None or submission.workflow_state == 'graded':
-            continue
-
-        for attachment in submission.attachments:
-            response = requests.get(attachment.url, allow_redirects=True)
-            if response.status_code != 200:
-                print(f"Failed to download file: {attachment['url']}")
-                continue
-
-            file_content = response.content.decode()
-
-            pos = 0
-            for word in search_words:
-                pos = file_content.find(word, pos)
-                if pos == -1:
-                    # Word not found, stop searching
-                    break
-                else:
-                    print(f"{word} - ", end="")
-
-            if (pos > 1):
-                print(f"OK {pos}")
-                print(f"Submission checked and correct, grading student: {submission.user['name']} (ID: {submission.user_id})")
-                rateSubmission(submission, rating, feedback)
-            else:
-                print(f"Not OK {pos}")
-                print(f"Submission incorrect, not grading student: {submission.user['name']} (ID: {submission.user_id})")
 
 def update_grade_and_feedback(posted_variables):
     output=''
@@ -241,24 +245,20 @@ def update_grade_and_feedback(posted_variables):
 
 @app.route('/')
 def index():
-    return redirect(url_for('section.section_list'))
+    results=loadFormData()
+    if results:
+        return render_template('rate.html', data=results)
+    
+    return render_template('results.html', data="No Data in form cache")
 
-@app.route('/correct/<assignment_id>')
-def correct(assignment_id):
-    data_cache='data_cache.json'
-    filename=data_cache
-    if os.path.isfile(filename):  # check if file/cache exists
-        with open(filename, 'r') as f: 
-            data = json.load(f) 
-    else:
-       return render_template('rate.html', data='') 
-    for item in data:
-        if ( int(assignment_id)==int(item['assignment_id']) ):
-            results=listUnratedAssignments(item)
-            return render_template('rate.html', data=results)
+@app.route('/<file_name>')
+def indexParameter(file_name):
+    results=loadFormData(file_name)
+    if results:
+        return render_template('rate.html', data=results)
+    
+    return render_template('results.html', data="File empty or deleted")
 
-    return "None"
-    return render_template('rate.html', data=results)
 
 @app.route('/correcta/<cohort>/<assignment_id>')
 def correcta(cohort, assignment_id):
@@ -292,9 +292,12 @@ def submit_ratings():
     results=update_grade_and_feedback(request.form)
     return render_template('results.html', data=results)
 
-@app.route('/test')
-def test():
-    return render_template('results.html', data='Empty data set')
+@app.route('/list')
+def list():
+    data=getFormdataFiles()
+    print(f"Data: {data}")
+    return render_template('list.html', data=data)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- virtualenv.bat
+++ virtualenv.bat
@@ -1,2 +1,5 @@
 @echo off
 C:\Users\maxbi\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\Scripts\virtualenv %*
+
+cd
+C:\Users\maxbi\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\Scripts\
