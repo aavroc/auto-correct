c5b5efc285cbfc307e475ba3cd7ec7135f9f0a5b
Max Bisschop <max@ffkijken.com>
2023-06-29 22:01:17 +0200

    rate button hide and other place

---
+++ myflaskapp/example-config.py
@@ -0,0 +1,6 @@
+config={
+    'API_URL':'https://edu.server.com',
+    'API_KEY':'12345....your-api-key',
+    'default_feedback_pos': ['Top gedaan!','Nice!'],
+    'default_feedback_neg': ['Niet helemaal goed','Nope']
+}
